3d1c04e7f2273abc2b45038d5a2b2f179a424207
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-03 22:14:10 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,3 +1,4 @@
+#this module is used because the method is in both the artist and song class
 module Findable
   def find_by_name(name)
     all.detect{|a| a.name == name}
@@ -5,7 +6,8 @@ module Findable
 
 end
 
+#Artist.find_by_name("Adele")
 #databases ex:
 #connects users to an online store
 #introspects on a class's .all method and extracts
-#the instance of the class with a certain name 
+#the instance of the class with a certain name
